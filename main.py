626cac0005890cbb7484204e0fe2cb868d5f192f
Alan MARCHAND <galan.marchand@outlook.fr>
2024-04-15 20:50:09 +0200

    Add logging

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 # -*- coding: utf-8 -*-
 from vlm.loadlibs_parser import LoadlibsParser, SectionError
+import logging
 import os
 import sys
 
+def configure_logging():
+    logging.basicConfig(filename='./vlm.log', 
+                        level=logging.DEBUG, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
 def main():
     
-    print(f'Current directory: {os.getcwd()}')
+    configure_logging()
+    logging.info(f'Beginning of processing of {os.path.abspath(sys.argv[0])}')
+    logging.info(f'Current directory: {os.getcwd()}')
 
     try:
         filename = "./data/vlm.txt"
         loadlibs_parser = LoadlibsParser(filename)
 
         for number_of_modules, loadlib_lines in loadlibs_parser:
-            print("Nombre de modules:", number_of_modules)
-            print(len(loadlib_lines))
-
+            loadlib_name = loadlib_lines[0].split('=')[1].split(',')[0]
+            message = f'{loadlib_name}'
+            message = f'Nombre de modules {str(number_of_modules).rjust(3)} '
+            message += f'Nombre de lignes par loadlib {str(len(loadlib_lines)).rjust(4)}'
+            logging.info(message)
+            
     except (FileNotFoundError, 
             PermissionError, 
             IsADirectoryError, 
             IOError, 
             UnicodeDecodeError) as e:
+        logging.error(f'{e}')
         print(e)
         sys.exit(1)
         
     except SectionError as se:
+        logging.error(f'{e}')
         print(se)
         sys.exit(1)
 
--- vlm/loadlibs_parser.py
+++ vlm/loadlibs_parser.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 from vlm.utils.file_handler import FileHandler
+
 class SectionError(Exception):
     """Exception raised for errors related to sections in the file."""
     def __init__(self, section_name):
         self.section_name = section_name
         super().__init__(section_name)
+
 class LoadlibsParser:
     def __init__(self, filename):
         self.filename = filename
-        self.file = FileHandler.open_file(self.filename)  # Ouverture du fichier
-        self.number_of_modules = 0
-        self.loadlib_name = ()   # nom de la loadlib en cours de traitement
-        self.loadlib_lines = ()  # Initialisation du tuple
+        self.file = FileHandler.open_file(self.filename)  # Open the file
+        self.loadlib_name = ''              # Name of the loadlib being processed
+        self.previous_loadlib_name = ''     # Name of the previous loadlib 
+        self.loadlib_lines = ()             # Tuple containing the lines of a loadlib
+        self.modules = 0                    # Number of modules declared in the FMNBB437 message
 
     def ignore_line(self, line):
         return (not line.strip() or
@@ -22,35 +25,50 @@ class LoadlibsParser:
 
     def __iter__(self):
         for line in self.file:
-            line = line[1:]  # Supprimer systématiquement le caractère ASA
+            line = line[1:]                 # Always remove the ASA character
 
             if self.ignore_line(line):
                 continue
             
             if line.startswith("$$FILEM VLM DSNIN="):
                 self.loadlib_name = line.split("=")[1].split(",")[0]
-                if self.loadlib_lines:  # Si loadlib_lines n'est pas vide
+                if self.loadlib_lines:      # If loadlib_lines is not empty
                     raise SectionError(
-                        f'Missing end section identification for loadlib '
-                        f'(FMNBB437 or FMNBE329).\nLast encountered start '
-                        f'section identification for loadlib: {self.loadlib_name}.'
+                        f'Missing end of loadlib section (FMNBB437 or FMNBE329).\n'
+                        f'Current loadlib is {self.loadlib_name}.\n'
+                        f'Previous encountered loadlib is {self.previous_loadlib_name}.'
                     )
 
-                    # yield (self.number_of_modules, self.loadlib_lines)
                 self.loadlib_lines = (line,)
             
             elif line.startswith("FMNBB437"):
-                self.number_of_modules = int(line.split()[2])
+                if not self.loadlib_lines:  # If loadlib_lines is not empty
+                    raise SectionError(
+                        f'Missing section identification for loadlib ($$FILEM VLM DSNIN).\n'
+                        f'Current loadlib is {self.loadlib_name}.\n'
+                        f'Previous encountered loadlib is {self.previous_loadlib_name}.'
+                    )
+                
+                self.modules = int(line.split()[1])
                 self.loadlib_lines += (line,)
-                yield (self.number_of_modules, self.loadlib_lines)
+                self.previous_loadlib_name = self.loadlib_name
+                yield (self.modules, self.loadlib_lines)
                 self.loadlib_lines = ()
             
             elif line.startswith("FMNBE329"):
+                if not self.loadlib_lines:  # If loadlib_lines is not empty
+                    raise SectionError(
+                        f'Missing section identification for loadlib ($$FILEM VLM DSNIN).\n'
+                        f'Current loadlib is {self.loadlib_name}.\n'
+                        f'Previous encountered loadlib is {self.previous_loadlib_name}.'
+                    )
+                
                 self.loadlib_lines += (line,)
+                self.previous_loadlib_name = self.loadlib_name
                 yield (0, self.loadlib_lines)
                 self.loadlib_lines = ()
             
             else:
                 self.loadlib_lines += (line,)
 
-        FileHandler.close_file(self.file)  # Fermeture du fichier
+        FileHandler.close_file(self.file)   # Close the file
